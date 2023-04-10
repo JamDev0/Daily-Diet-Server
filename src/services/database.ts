379508c7bdee59ae7c32bc57e8857e4d2de6888f
JamDev0 <jamdeveloper0@gmail.com>
2023-04-10 08:06:53 -0300

    Feature: Initial knex db config

---
+++ src/services/database.ts
@@ -0,0 +1,14 @@
+import { Knex, knex as knexSetup } from 'knex';
+import { env } from '../../env';
+
+export const config: Knex.Config = {
+  client: env.DATABASE_ENGINE,
+  connection: env.DATABASE_URL,
+  useNullAsDefault: true,
+  migrations: {
+    directory: './database/migrations',
+    extension: '.ts',
+  }
+};
+
+export const knex = knexSetup(config);
