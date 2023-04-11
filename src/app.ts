1ad3e1ec313f6d21130eee37f2646e017e9ac62a
JamDev0 <jamdeveloper0@gmail.com>
2023-04-11 12:27:18 -0300

    Feature: implementation of the meals route and cookiesPlugin

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,11 @@
-import fastify from "fastify";
+import cookiesPlugin from '@fastify/cookie';
+import fastify from 'fastify';
+import { mealsRoutes } from './routes/meals';
 
 export const app = fastify();
+
+app.register(cookiesPlugin);
+
+app.register(mealsRoutes, {
+  prefix: 'meals'
+});
