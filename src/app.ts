4e6b59277fb73dfaef154caf9c8741d011c94dc6
JamDev0 <jamdeveloper0@gmail.com>
2023-04-13 11:45:00 -0300

    Feature: implementation of the uses routes and services routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import cookiesPlugin from '@fastify/cookie';
 import fastify from 'fastify';
 import { mealsRoutes } from './routes/meals';
+import { servicesRoutes } from './routes/services';
+import { usersRoutes } from './routes/users';
 
 export const app = fastify();
 
@@ -8,4 +10,12 @@ app.register(cookiesPlugin);
 
 app.register(mealsRoutes, {
   prefix: 'meals'
+});
+
+app.register(usersRoutes, {
+  prefix: 'users'
+});
+
+app.register(servicesRoutes, {
+  prefix: 'services'
 });
