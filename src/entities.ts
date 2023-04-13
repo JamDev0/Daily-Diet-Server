872e3e46892fd6238986563422567a9b5efdced5
JamDev0 <jamdeveloper0@gmail.com>
2023-04-13 09:05:45 -0300

    Feature: new entities entries

--- src/entities.ts
+++ src/entities.ts
@@ -6,3 +6,15 @@ export interface Meal {
   date: Date;
   is_compliant: boolean;
 }
+
+export interface User {
+  id: string;
+  user_name: string;
+  password: string;
+}
+
+export interface Session_id {
+  value: string;
+  user_id: string;
+  expire_date: Date;
+}
