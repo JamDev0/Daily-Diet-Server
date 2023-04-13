92739d654848b6a837368e598e1a7fd9ca50332b
JamDev0 <jamdeveloper0@gmail.com>
2023-04-13 16:48:57 -0300

    Fix: wrong table name

--- database/migrations/20230413102902_create-user-auth.ts
+++ database/migrations/20230413102902_create-user-auth.ts
@@ -18,7 +18,7 @@ export async function up(knex: Knex): Promise<void> {
 
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTable('user');
+  await knex.schema.dropTable('users');
 
   await knex.schema.dropTable('session_ids');
 }
