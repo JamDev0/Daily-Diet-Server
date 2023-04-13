a5d5f09c6e1a3df7f3a916de8179e556805053fb
JamDev0 <jamdeveloper0@gmail.com>
2023-04-13 09:06:23 -0300

    Feature: new tables type definition

---
+++ database/migrations/20230413102902_create-user-auth.ts
@@ -0,0 +1,25 @@
+import { Knex } from 'knex';
+
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('user', (table) => {
+    table.uuid('id').primary().index();
+    table.string('user_name').notNullable();
+    table.string('password').notNullable;
+  });
+
+  await knex.schema.createTable('session_ids', (table) => {
+    table.uuid('value').primary().index();
+    table.uuid('user_id').unsigned().notNullable().index();
+    table.foreign('user_id').references('id').inTable('user').onDelete('CASCADE').onUpdate('CASCADE');
+    table.date('expire_date').notNullable();
+  });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('user');
+
+  await knex.schema.dropTable('session_ids');
+}
+
--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -2,10 +2,12 @@ import { Knex } from "knex"; // eslint-disable-line
 
 Knex; // Needed so vs code dosen't erase the line above (Even with the eslint-disable-line )
 
-import { Meal } from './../entities';
+import { Meal, Session_id, User } from './../entities';
 
 declare module 'knex/types/tables' {
   export interface Tables {
     meals: Meal;
+    users: User;
+    session_ids: Session_id;
   }
 }
