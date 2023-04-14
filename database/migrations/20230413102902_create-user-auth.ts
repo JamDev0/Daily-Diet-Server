b63985fcd18d83a8c79d140c32ad380f87fbb2af
JamDev0 <jamdeveloper0@gmail.com>
2023-04-14 19:44:12 -0300

    Fix: wrong table name in migration

--- database/migrations/20230413102902_create-user-auth.ts
+++ database/migrations/20230413102902_create-user-auth.ts
@@ -11,7 +11,7 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('session_ids', (table) => {
     table.uuid('value').primary().index();
     table.uuid('user_id').unsigned().notNullable().index();
-    table.foreign('user_id').references('id').inTable('user').onDelete('CASCADE').onUpdate('CASCADE');
+    table.foreign('user_id').references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE');
     table.date('expire_date').notNullable();
   });
 }
